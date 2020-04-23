493eebf67dfe63e4c82c866578f9c3ffe0bf3011
Christian <cgcandido@up.edu.ph>
2020-04-24 07:16:23 +0800

    Push

--- app/src/main/java/com/example/tracker/utils/Constants.java
+++ app/src/main/java/com/example/tracker/utils/Constants.java
@@ -3,4 +3,5 @@ package com.example.tracker.utils;
 public class Constants {
     public static String KEY_DATEOFBIRTH = "dateOfBirth";
     public static String KEY_UNIQUEID = "uniqueId";
+    public static String ACTION_START_SERVICE = "LocationService";
 }
